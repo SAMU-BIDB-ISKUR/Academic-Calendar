77cfcfb068d6dffcacffb76fdcbd10fdcaabf261
241478040 <241478040@samsun.edu.tr>
2025-12-05 14:22:46 +0300

    Update AcademicYearRepository.cs

--- AcademicCalendar/Modules/AcademicYear/Repository/AcademicYearRepository.cs
+++ AcademicCalendar/Modules/AcademicYear/Repository/AcademicYearRepository.cs
@@ -1,6 +1,8 @@
-﻿using AcademicCalendar.Data;
-using AcademicCalendar.Modules.AcademicYear.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using AcademicCalendar.Data;
+using AYModel = AcademicCalendar.Modules.AcademicYear.Model.AcademicYear;
 
 namespace AcademicCalendar.Modules.AcademicYear.Repository
 {
@@ -13,21 +15,52 @@ namespace AcademicCalendar.Modules.AcademicYear.Repository
             _context = context;
         }
 
-        public async Task<List<AcademicYear>> GetAllAsync()
+        public async Task<List<AYModel>> GetAllAsync()
         {
             return await _context.AcademicYears.ToListAsync();
         }
 
-        public async Task<AcademicYear?> GetByIdAsync(int id)
+        public async Task<AYModel?> GetByIdAsync(int id)
         {
             return await _context.AcademicYears.FindAsync(id);
         }
 
-        public async Task<AcademicYear> CreateAsync(AcademicYear year)
+        public async Task<AYModel> CreateAsync(AYModel year)
         {
             _context.AcademicYears.Add(year);
             await _context.SaveChangesAsync();
             return year;
+        }
+
+        public async Task<bool> UpdateAsync(AYModel year)
+        {
+            _context.Entry(year).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (await _context.AcademicYears.FindAsync(year.Id) == null)
+                {
+                    return false;
+                }
+                throw;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var yearToDelete = await _context.AcademicYears.FindAsync(id);
+            if (yearToDelete == null)
+            {
+                return false;
+            }
+
+            _context.AcademicYears.Remove(yearToDelete);
+            await _context.SaveChangesAsync();
+            return true;
         }
     }
 }
